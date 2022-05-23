69e502888da1ba490507ad7db82529564361eba8
colin brindle <hcolinbrindle@gmail.com>
2022-05-23 17:54:34 -0400

    Updates

---
+++ precache-manifest.0a82d7698772502ac35f4cc45ec3741e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21dd009f014d353a368",
+    "url": "/colinbrindle/react-portfolio/static/css/main.41781f8b.chunk.css"
+  },
+  {
+    "revision": "a21dd009f014d353a368",
+    "url": "/colinbrindle/react-portfolio/static/js/main.a21dd009.chunk.js"
+  },
+  {
+    "revision": "67e978e5961f463edf65",
+    "url": "/colinbrindle/react-portfolio/static/css/1.e1c31a22.chunk.css"
+  },
+  {
+    "revision": "67e978e5961f463edf65",
+    "url": "/colinbrindle/react-portfolio/static/js/1.67e978e5.chunk.js"
+  },
+  {
+    "revision": "9c50694c81c184fbce6d",
+    "url": "/colinbrindle/react-portfolio/static/js/runtime~main.9c50694c.js"
+  },
+  {
+    "revision": "27de5099fa2d649bcfa245be80381003",
+    "url": "/colinbrindle/react-portfolio/index.html"
+  }
+];
